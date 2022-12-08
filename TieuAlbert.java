15055aba894f84d85da893be5dd5f62ba094112b
Albert Tieu <albert.tieu@mrjaffesclass.com>
2022-12-08 13:47:54 -0800

    done

---
+++ TieuAlbert.java
@@ -0,0 +1,29 @@
+
+/**
+ * A BoopleSnoot is Mr. Jaffe's animal
+ * 
+ * @author Mr. Jaffe
+ * @version 1.0 2017-07-14
+ */
+public class TieuAlbert extends Animal
+{
+  public TieuAlbert() {
+    super("Albert Tieu", "blue", 3);
+  }
+  
+  public String getType() {
+    return "Grumbob";
+  }
+  
+  public String getName() {
+      return "Timothy";
+  }
+  
+  public String speak() {
+      return "GRAAAHHHHH";
+  }
+  
+  public String getDisposition() {
+      return "very friendly";
+  }
+}
